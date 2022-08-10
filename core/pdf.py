989d314f801d00dc914139c5cb00914868a9470a
CodeWithEhtisham <shamikhan0347@gmail.com>
2022-08-10 18:12:32 +0500

    working on pdf maker

---
+++ core/pdf.py
@@ -0,0 +1,65 @@
+from PyPDF2 import PdfReader
+import fitz
+import io
+from PIL import Image
+import tqdm
+FILE_TYPE = ['pdf']
+def getImage(pdf_path:str):
+    # pdf_file = fitz.open(pdf_path)
+    # page = pdf_file[0]
+    # print(pdf_file)
+    # img_list=page.getImageList()
+    # for img in img_list:
+    #     pix=img[1]
+    #     pix.writePNG("images/{}.png".format(img[0]))
+        if ".pdf" in pdf_path:
+            doc = fitz.Document(pdf_path)
+            # for i in range(len(doc)):
+            for i,img in enumerate(doc.get_page_images(0)):
+                    xref = img[0]
+                    image = doc.extract_image(xref)
+                    pix = fitz.Pixmap(doc, xref)
+                    pix.save("core/static/images/{}.png".format(i))
+                    
+        print("Done!")
+
+
+
+def JsonParser(pdf: PdfReader):
+    obj={
+        
+    }
+    for page in pdf.pages:
+        text=page.extractText()
+        for index,line in enumerate(text.split('\n')):
+            if "National" in line.split(' '):obj["NationalID"]=line.split(' ')[-1]
+            elif "Pin" in line.split(' '):obj["Pin"]=line.split(' ')[-1]
+            elif "Name(Bangla)" in line.split(' '):obj["NameBangla"]=" ".join(line.split(' ')[2:])
+            elif "Name(English)" in line.split(' '): obj["NameEnglish"]=" ".join(line.split(' ')[1:])
+            elif ['Date', 'of', 'Birth'] == line.split()[:3]: obj["DOB"]=line.split(' ')[-1]
+            elif ['Father','Name'] == line.split()[:2]: obj["FatherName"]=" ".join(line.split(' ')[2:])
+            elif ['Mother','Name'] == line.split()[:2]: obj["MotherName"]=" ".join(line.split(' ')[2:])
+            elif ['Brith','Place'] == line.split()[:2]: obj["BirthPlace"]=" ".join(line.split(' ')[2:])
+            elif "Village/R" in line.split():
+                village=line.split("oad")[-1].split("Home")[0]
+                home=text.split('\n')[index+1][2:].split(' ')
+                home.append(text.split('\n')[index+2].split(' ')[0])
+                obj["Village"]=village
+                obj["Home"]=" ".join(home)
+            elif ['Post','Office'] == line.split()[:2]:
+                post=line.split('Office')[-1].split('Postal')[0]
+                obj["PostOffice"]=post
+                
+            
+    return obj
+def GetPdf(file_path:str):
+    pdf = PdfReader(file_path)
+    jsonobj=JsonParser(pdf)
+    print(jsonobj)
+    return jsonobj
+
+
+if __name__ == '__main__':
+    # getImage('media/0মোঃ রফিকুল ইসলাম (1)_2cMxbyh.pdf')
+    GetPdf('media/0মোঃ রফিকুল ইসলাম (1)_2cMxbyh.pdf')
+    
--- core/views.py
+++ core/views.py
@@ -6,38 +6,42 @@ from django.core.files.storage import FileSystemStorage
 from django.contrib import messages
 from .models import Upload_File
 from PyPDF2 import PdfReader
+import fitz
+import io
+from PIL import Image
 
 FILE_TYPE = ['pdf']
+def getImage(pdf_path:str):
+    pdf_file = fitz.open(pdf_path)
+    page = pdf_file[0]
+    img_list=page.getImageList()
+    for img in img_list:
+        pix=img[1]
+        pix.writePNG("images/{}.png".format(img[0]))
+    
+def JsonParser(pdf: PdfReader):
+    return pdf.pages[0].extractText()
+def GetPdf(file_path:str):
+    pdf = PdfReader(file_path)
+    jsonobj=JsonParser(pdf)
+    print(jsonobj)
+    return jsonobj
+
+
 
 def index(request):
     # form = Upload_Form()
     if request.method == 'POST':
         f=request.FILES["pdf_file"]
         fs = FileSystemStorage()
-        filename = fs.save(f.name, f)
-        uploaded_file_url = fs.url(filename)
-
-        if request.POST.get('upload_button'):
-            print("POST Upload File ")
-            if filename not in FILE_TYPE:
-                messages.error(request, 'Please upload a .pdf file.')
-            
-            file_data = Upload_File(pdf_file=filename)
-            file_data.save()
-            return HttpResponse('<h1>File Uploaded Successfully!</h1>')
-
-        if request.POST.get('generate_button', False):
-            pdf = Upload_File.objects.all().first()
-            reader = PdfReader(settings.MEDIA_ROOT + '/' + pdf.pdf_file.name)
-            text = ""
-            for page in reader.pages:
-                text += page.extractText()
-            print(text)
+        fs.url = fs.base_url + f.name
+        if fs.url.split('.')[-1] not in FILE_TYPE:
+            messages.error(request, 'File type not allowed')
+            return HttpResponseRedirect(reverse('index'))
+        else:
+            fs.save(f.name, f)
+            print(fs.url)
+            messages.success(request, 'File uploaded successfully')
+            return HttpResponseRedirect(reverse('index'))
     else:
-        # pdf = Upload_File.objects.all().last()
-        # reader = PdfReader(settings.MEDIA_ROOT + '/' + pdf.pdf_file.name)
-        # text = ""
-        # for page in reader.pages:
-        #     text += page.extractText()
-        # print(text)
         return render(request, 'index.html')

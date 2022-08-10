21e79ca8835475a8666275100020e05743b8934d
abidkhan03 <khansahb5566@gmail.com>
2022-08-10 13:16:56 +0500

    Uplode pdf file function successfuly added to database

--- core/views.py
+++ core/views.py
@@ -4,38 +4,40 @@ from django.urls import reverse
 from django.conf import settings
 from django.core.files.storage import FileSystemStorage
 from django.contrib import messages
-from .forms import Upload_Form
 from .models import Upload_File
+from PyPDF2 import PdfReader
 
 FILE_TYPE = ['pdf']
 
 def index(request):
     # form = Upload_Form()
     if request.method == 'POST':
-        # f=request.FILES["pdf_file"]
-        # fs = FileSystemStorage()
-        # filename = fs.save(f.name, f)
-        # uploaded_file_url = fs.url(filename)
-        file_ = request.FILES['pdf_file']
-        print("POST POST POST ")
-        if request.POST.get('upload_file'):
+        f=request.FILES["pdf_file"]
+        fs = FileSystemStorage()
+        filename = fs.save(f.name, f)
+        uploaded_file_url = fs.url(filename)
 
-            if not file_.name.endswith('.pdf'):
+        if request.POST.get('upload_button'):
+            print("POST Upload File ")
+            if filename not in FILE_TYPE:
                 messages.error(request, 'Please upload a .pdf file.')
             
-            file_data = Upload_File(pdf_file=file_)
+            file_data = Upload_File(pdf_file=filename)
             file_data.save()
+            return HttpResponse('<h1>File Uploaded Successfully!</h1>')
 
-            # form = Upload_Form(request.POST, request.FILES)
-            # if form.is_valid():
-            #     user_pr = form.save(commit=False)
-            #     user_pr.pdf_file = request.FILES['pdf_file']
-            #     file_type = user_pr.pdf_file.name.split('.')[1]
-            #     file_type = file_type.lower()
-            #     if file_type not in FILE_TYPE:
-            #         messages.error(request, "File must in pdf format! Please Try again")
-            # user_pr.save()
-            # print("data saved")
-            return render(request, 'index.html') 
+        if request.POST.get('generate_button', False):
+            pdf = Upload_File.objects.all().first()
+            reader = PdfReader(settings.MEDIA_ROOT + '/' + pdf.pdf_file.name)
+            text = ""
+            for page in reader.pages:
+                text += page.extractText()
+            print(text)
     else:
+        # pdf = Upload_File.objects.all().last()
+        # reader = PdfReader(settings.MEDIA_ROOT + '/' + pdf.pdf_file.name)
+        # text = ""
+        # for page in reader.pages:
+        #     text += page.extractText()
+        # print(text)
         return render(request, 'index.html')
